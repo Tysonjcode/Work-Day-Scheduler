c887915093b4091639c86b278b662f2c633f0e9c
Tyson Jackson <tyson@alittlerdesigner>
2022-10-06 22:54:17 +0800

    fishing building app using only JS additions still some fixes to do

---
+++ assets/js/script.js
@@ -0,0 +1,70 @@
+
+function getLocalStorage(key) {
+    let value = localStorage.getItem(key);
+    if (value) {
+        $(`#text${key}`).text(value);
+    }
+}
+// This is inpuuting the current day and date 
+$(document).ready(function() {
+    $("#currentDay").text(moment().format("dddd, MMMM Do"));
+    for (let i = 9; i < 18; i++) {
+    
+        // row
+        var row = $(`<div data-time=${i} id='${i}' class="row">`);
+
+        // column
+        var col1 = $('<div class="col-sm-2"> <p class="hour">' + formatAMPM(i) + '</p>');
+
+        // column 2
+        var col2 = $(`<div class="col-sm-8 past"><textarea id=text${i} class="description" placeholder="Add your event here..."></textarea>`);        
+       
+        // column 3
+        var col3 = $(`<div class="col-sm-2"><button class="saveBtn" id=${i}><i class="fas fa-save"></i></button>`)
+        
+        // col to row
+        row.append(col1);
+        row.append(col2);
+        row.append(col3);
+
+        // adding the rows to container to page
+        $(".container").append(row);
+
+        getLocalStorage(i);
+    }
+        // using formatAMPN(hours) do build the times for the rows
+    function formatAMPM(hours) {
+        var ampm = hours >= 12 ? 'pm' : 'am';
+        hours = hours % 12;
+        hours = hours ? hours : 12;
+        return hours + ampm;
+    }
+formatAMPM();
+
+//Buildeing the colors of each hour to change as the day goes on
+function updateColors(){
+        var currentTime = new Date().getHours();
+        for (var i = 9; i < 18; i++) { 
+        console.log(currentTime, $(`#${i}`).data("time"));
+         if ($(`#${i}`).data("time") == currentTime){
+            $(`#text${i}`).addClass( "present");
+        } else if (currentTime < $(`#${i}`).data("time")) {
+            $(`#text${i}`).addClass( "future");
+        }
+    }
+}
+
+setInterval(function() {
+    updateColors();
+}, 1000);
+// Creating save button to save to local storage
+var saveBtn = $('.saveBtn');
+saveBtn.on('click', function(){
+    let eventId = $(this).attr('id');
+    let eventText = $(this).parent().siblings().children('.description').val();
+    localStorage.setItem(eventId, eventText);
+});});
+
+// To do 
+// Want to fix save button size to be the same as the row 
+// Want to increase the size of the text box to cover the whole inside of the middle container
